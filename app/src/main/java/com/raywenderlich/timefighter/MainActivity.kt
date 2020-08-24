2b70e2cc20570e3db5b9f689999a7c03965171fb
chickadeepend <arnbristerdjr@gmail.com>
2020-08-23 21:18:22 -0400

    added timer functionality and a boolean variable to start the game

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.raywenderlich.timefighter
 
+import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 
@@ -12,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tapMeButton: Button
 
     private var score = 0
+    private  var gameStarted = false
+
+    private lateinit var countDownTimer: CountDownTimer
+    private var initialCountDown: long = 60000
+    private var countDownInterval: long = 1000
+    private var timeLeft = 60
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +39,36 @@ class MainActivity : AppCompatActivity() {
     private  fun incrementScore() {
         score++
 
-        val newScore = "Your Score: $score"
+        val newScore = getString(R.string.your_score, score)
         gameScoreTextView.text = newScore
     }
 
     private fun resetGame() {
-        //TODO reset game logic
+        //1
+        score = 0
+
+        val initiateScore = getString(R.string.your_score, score)
+        gameScoreTextView.text = initiateScore
+
+        val initialTimeLeft = getString(R.string.time_left, 60)
+        timeLeftTextView.text = initialTimeLeft
+
+        //2
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: long) {
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString = getString(R.string.time_left, timeLeft)
+                timeLeftTextView.tect = timeLeftString
+            }
+
+            override fun onFinish() {
+                //TODO implemented late
+            }
+        }
+
+        //4
+        gameStarted = false
     }
 
     private fun startGame() {
