40bb0354f92a54cae23af48ce8eb9a583b23c30d
chickadeepend <arnbristerdjr@gmail.com>
2020-08-23 20:38:18 -0400

    added values to score variable. added button functionality to the tap me button

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -11,15 +11,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var timeLeftTextView: TextView
     private lateinit var tapMeButton: Button
 
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO connect views to variables
+        //1
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+
+        tapMeButton = findViewById(R.id.tap_me_button)
+
+        //2
+        tapMeButton.setOnClickListener { incrementScore() }
     }
 
     private  fun incrementScore() {
-        //TODO increment score logic
+        score++
+
+        val newScore = "Your Score: $score"
+        gameScoreTextView.text = newScore
     }
 
     private fun resetGame() {
