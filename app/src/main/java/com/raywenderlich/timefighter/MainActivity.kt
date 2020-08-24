013b0c6c0add3c8b5850e78186f9588400b368af
chickadeepend <arnbristerdjr@gmail.com>
2020-08-23 20:32:34 -0400

    outlined the class to set up logic for the button, start game, end game, rand reset game

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -2,10 +2,35 @@ package com.raywenderlich.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO connect views to variables
+    }
+
+    private  fun incrementScore() {
+        //TODO increment score logic
+    }
+
+    private fun resetGame() {
+        //TODO reset game logic
+    }
+
+    private fun startGame() {
+        //TODO start game logic
+    }
+
+    private fun endGame() {
+        //TODO end game logic
     }
 }
